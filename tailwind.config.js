6578c3ace3351b37b2322609d53cc42fc931c5b4
Masum Hosen <masumhosen4744@gmail.com>
2025-08-02 23:51:34 +0600

    add for test

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// /**@type {import('tailwindcss').config} */
+// Module.exports = {
+//     content: ["./src/**/*.{html,js"],
+//     theme: {
+//         extend: {},
+//     },
+//     Plugins: [],
+// }
