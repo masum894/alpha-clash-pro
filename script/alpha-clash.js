86814965deb9686e572bd5e7f91dbe56df970e79
Masum Hosen <masumhosen4744@gmail.com>
2025-08-08 00:45:00 +0600

    all most done

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -7,8 +7,66 @@ function alphabet() {
     
 }
 
+document.addEventListener('keyup', eventListener);
+
+function eventListener(element) {
+    const showAlphabet = document.getElementById('current-alphabet');
+    const getTotalScore = document.getElementById('total-score');
+    const showAlphabetInnerTex = showAlphabet.innerText;
+     const allphabet = showAlphabetInnerTex.toLowerCase();
+    const typeAlphabet = element.key;
+   
+     if (allphabet === typeAlphabet) {
+        // console.log('matched');
+
+        // get id
+        const getScore = scoreId('score');
+        // innerText,parseInt and set a new value
+        // console.log(getScore);
+        
+        const score = getScore + 1;
+        // console.log(score);
+        
+        //   show on the right place
+        setValue('score', score);
+
+        alphabet();
+        removeBg(allphabet);
+     }
+     else{
+        // get id,innerText and parseInt by a function
+        const getLife = scoreId('life');
+        //  set a new value
+        const life = getLife - 1;
+        // show on the right place
+        setValue('life', life);
+
+        // try 
+        if (life === 0) {
+            gameOver();
+        }
+        
+     }
+    
+}
+
+
 function play() {
     hideElement('home');
     ShowElement('play-ground');
+    hideElement('score-board');
+    setValue('life', 5);
+    setValue('score', 0);
+    // removeBg(allphabet);
+    
     alphabet()
+}
+function gameOver() {
+   hideElement('play-ground');
+   ShowElement('score-board'); 
+   const lastScore = scoreId('score');
+    setValue('total-score', lastScore);
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBg(currentAlphabet);
+
 }
--- script/utility.js
+++ script/utility.js
@@ -8,11 +8,35 @@ function ShowElement(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden'); 
 }
-
+// add bg
 function setAlphabet(elementId) {
     const getId = document.getElementById(elementId);
     getId.classList.add('bg-orange-400')
     
+}
+// remove bg
+function removeBg(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400')
+}
+
+// get score id
+function scoreId(elementId) {
+    const getScoreId = document.getElementById(elementId);
+    const scoreText = getScoreId.innerText;
+    const currentScore = parseInt(scoreText);
+    return currentScore;
+}
+
+function setValue(elementId, value) {
+const element = document.getElementById(elementId);
+element.innerText = value;
+    
+}
+function getElementTextById(elementId) {
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
 }
 
 function getAlphabet() {
